102a02335b208ec6db483f829bedbb5c621fc7ba
Shane <shanemcneil19@gmail.com>
2020-07-21 21:01:06 -0700

    add first meeting minutes to Minutes.js

---
+++ src/Data/Minutes.js
@@ -0,0 +1,116 @@
+const Minutes = [
+    {
+        id: 1,
+        date: {
+            month: "September",
+            day: "6th",
+            year: "2018"
+        },
+        intro: [
+            "Pres. Ken Sime called the meeting to order with the Pledge of Allegiance.",
+            "The minutes of the April meeting were read by Sec. Sharon Hauser.  Minutes were approved and accepted.",
+            "Treasurer, Maria Renda, reported a balance of $269.77 as of April 1, 2018.  Additional income of $27 from the 50/50 drawing leaving a balance of $296.77 on April 30th. The Treasurer’s report was approved and accepted."
+        ],
+        oldBusiness: [
+            "Ken reported on AAHMO and recommended that all residents join at AAHMO.org. Residents can join for $60 for 2 years if joining by Nov.",
+            "It was suggested that there continues to be a problem with Management parking golf carts around fountain at night. Issue will again be brought to management.",
+            "It was requested that minutes of meeting be put on web site."
+        ],
+        newBusiness: [
+            "Pauline Werner reported that the next Neighborhood Watch Pot Luck dinner will be Sat. Nov. 3rd from 6 to 7 in the card rooms. Pauline will be moving and requested that someone take over her responsibilities."
+        ],
+        residentComments: [
+            "A resident requested that the Bocca Ball courts should receive more attention as they are not being taken care of.",
+            "A resident asked why the Residents meeting is not published in the Thesman newspaper.",
+            "He also requested that an article on Volunteers who go above and beyond be recognized each month in the paper.",
+            "It was reported that there are several instances of the guards at the Ellsworth gate not getting up and waving residents in. Residents reported not be able to see if the guard waves them on through.",
+            "During a discussion of the duties of the Board, it was recommended that residents contact Kerrin, in the construction office, with complaints/issues or write directly to Mr. Thesman in the California office. It was also suggested that residents can go directly to locate county or city agencies with issues of concern.",
+            "All issues will be brought before Management.",
+            "The meeting was adjourned."
+        ],
+        nextMeeting: "",
+        submittedBy: ""
+    },
+    {
+        id: 2,
+        date: " 2018",
+        intro: [],
+        oldBusiness: [],
+        newBusiness: [],
+        residentComments: [],
+        nextMeeting: "",
+        submittedBy: ""
+    },
+    {
+        id: 3,
+        date: " 2018",
+        intro: [],
+        oldBusiness: [],
+        newBusiness: [],
+        residentComments: [],
+        nextMeeting: "",
+        submittedBy: ""
+    },
+    {
+        id: 4,
+        date: " 2018",
+        intro: [],
+        oldBusiness: [],
+        newBusiness: [],
+        residentComments: [],
+        nextMeeting: "",
+        submittedBy: ""
+    },
+    {
+        id: 5,
+        date: " 2018",
+        intro: [],
+        oldBusiness: [],
+        newBusiness: [],
+        residentComments: [],
+        nextMeeting: "",
+        submittedBy: ""
+    },
+    {
+        id: 6,
+        date: " 2018",
+        intro: [],
+        oldBusiness: [],
+        newBusiness: [],
+        residentComments: [],
+        nextMeeting: "",
+        submittedBy: ""
+    },
+    {
+        id: 7,
+        date: " 2018",
+        intro: [],
+        oldBusiness: [],
+        newBusiness: [],
+        residentComments: [],
+        nextMeeting: "",
+        submittedBy: ""
+    },
+    {
+        id: 8,
+        date: " 2018",
+        intro: [],
+        oldBusiness: [],
+        newBusiness: [],
+        residentComments: [],
+        nextMeeting: "",
+        submittedBy: ""
+    },
+    {
+        id: 9,
+        date: " 2018",
+        intro: [],
+        oldBusiness: [],
+        newBusiness: [],
+        residentComments: [],
+        nextMeeting: "",
+        submittedBy: ""
+    }
+]
+
+export default Minutes;
