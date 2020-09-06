de0f5efa4d11cd4fbcd2775e299be5601bfde64c
Shane McNeil <shanemcneil19@gmail.com>
2020-09-06 10:12:05 -0700

    add documents to elections page

--- src/routes/ElectionsPage/ElectionsPage.js
+++ src/routes/ElectionsPage/ElectionsPage.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { StyledElections } from './ElectionsPage.style';
 import purpose from '../../images/Purpose_of_Elections.pdf';
+import duties from '../../images/Board_Officer_Duties.pdf';
+import questionnaire from '../../images/Candidate_Questionnaire_2020.pdf';
+import committees from '../../images/COMMITTEES_AND_DUTIES.pdf';
+import notice from '../../images/Election_Meeting_Notice_2020.pdf';
 
 export default class ElectionsPage extends Component {
     render() {
@@ -8,6 +12,18 @@ export default class ElectionsPage extends Component {
             <StyledElections>
                 <a href={purpose} target="_blank" rel="noopener noreferrer">
                     Purpose of Elections
+                </a>
+                <a href={notice} target="_blank" rel="noopener noreferrer">
+                    Notice of Upcoming Election
+                </a>
+                <a href={questionnaire} target="_blank" rel="noopener noreferrer">
+                    Questionnaire for Potential Candidates
+                </a>
+                <a href={duties} target="_blank" rel="noopener noreferrer">
+                    Duties of Board Officers
+                </a>
+                <a href={committees} target="_blank" rel="noopener noreferrer">
+                    Current Committees and Duties
                 </a>
             </StyledElections>
         )
