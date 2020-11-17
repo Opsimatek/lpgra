c25a97784a87c912672cfd73147abc78d96994de
Shane McNeil <shanemcneil19@gmail.com>
2020-11-16 17:18:14 -0700

    update minutes november 2020

--- src/routes/MinutesListPage/MinutesListPage.js
+++ src/routes/MinutesListPage/MinutesListPage.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-expressions */
 import React, { Component } from 'react';
 import MinutesData from '../../Data/MinutesData';
-import schedule from '../../images/2020_Meetings.pdf';
+import schedule from '../../images/2021_Meetings.pdf';
 import { Minutes } from '../../Components';
 import { StyledMinutesList } from './MinutesListPage.style';
 import october2020Minutes from '../../images/LPGRA_Minutes_of_Meeting_Oct_1_2020.pdf';
+import november2020Minutes from '../../images/LPGRA_Minutes_November_5_2020.pdf';
 
 export default class MinutesListPage extends Component {
     renderMinutes() {
@@ -20,8 +21,9 @@ export default class MinutesListPage extends Component {
     render() {
         return (
             <StyledMinutesList>
-                <a href={schedule}>Click here for 2020 Meeting Schedule</a>
+                <a href={schedule}>Click here for 2021 Meeting Schedule</a>
                 <h2>Meetings Minutes List</h2>
+                <a href={november2020Minutes} target="_blank" rel="noopener noreferrer">November 2020</a>
                 <a href={october2020Minutes} target="_blank" rel="noopener noreferrer">October 2020</a>
                 {this.renderMinutes()}
             </StyledMinutesList>
